e52d699a462fdf9d58bebf040f58ed1fceb031a3
Akekapon Suebprom <akekapon.sueb@gmail.com>
2018-04-18 14:00:35 +0700

    Initial Commit

---
+++ ProcessBackground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProcessBackground
+//
+//  Created by Akekapon Suebprom on 18/4/2561 BE.
+//  Copyright © 2561 Suebprom Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
